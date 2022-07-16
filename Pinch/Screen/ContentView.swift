3d0a3fde045d8c157a18ea642f48b9869c5e67d9
Bagus Kurnia <baguskurnia1010@gmail.com>
2022-07-16 19:18:26 +0700

    Add long press gesture and floating scale and offset info

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -19,57 +19,65 @@ struct ContentView: View {
     }
   }
   
-    var body: some View {
-      NavigationView {
-        ZStack {
-          Image("magazine-front-cover")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .cornerRadius(10)
-            .padding()
-            .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-            .opacity(isAnimating ? 1 : 0)
-            .offset(x: imageOffset.width, y: imageOffset.height)
-            .animation(.linear(duration: 1), value: isAnimating)
-            .scaleEffect(imageScale)
-            // Tap Gesture
-            .onTapGesture(count: 2, perform: {
-              if imageScale == 1 {
-                withAnimation(.spring()) {
-                  imageScale = 5
-                }
-              } else {
-                resetImage()
+  var body: some View {
+    NavigationView {
+      ZStack {
+        Color.clear
+        
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
+          .animation(.linear(duration: 1), value: isAnimating)
+          .scaleEffect(imageScale)
+          // Tap Gesture
+          .onTapGesture(count: 2, perform: {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
               }
-            })
-            // Drag Gesture
-            .gesture(
-              DragGesture()
-                .onChanged { value in
-                  withAnimation(.linear(duration: 1)) {
-                    imageOffset = value.translation
-                  }
+            } else {
+              resetImage()
+            }
+          })
+          // Drag Gesture
+          .gesture(
+            DragGesture()
+              .onChanged { value in
+                withAnimation(.linear(duration: 1)) {
+                  imageOffset = value.translation
                 }
-                .onEnded { _ in
-                  if imageScale <= 1 {
-                    resetImage()
-                  }
+              }
+              .onEnded { _ in
+                if imageScale <= 1 {
+                  resetImage()
                 }
-            )
-        }
-        .navigationTitle("Pinch & Zoom")
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear(perform: {
-          isAnimating = true
-        })
+              }
+          )
       }
-      .navigationViewStyle(.stack)
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear(perform: {
+        isAnimating = true
+      })
+      .overlay(
+        InfoPanelView(scale: imageScale, offset: imageOffset)
+          .padding(.horizontal)
+          .padding(.top, 30)
+        , alignment: .top
+      )
     }
+    .navigationViewStyle(.stack)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
       ContentView()
-        .preferredColorScheme(.dark)
+        .preferredColorScheme(.light)
     }
 }
